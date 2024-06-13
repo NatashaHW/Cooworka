b8ff4b64c5afc558e7be38d296c712f73627280a
NatashaHW <165746907+NatashaHW@users.noreply.github.com>
2024-06-13 09:51:57 +0700

    Add files via upload

---
+++ Cooworka/Cooworka/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cooworka
+//
+//  Created by Natasha Hartanti Winata on 13/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Cooworka/Cooworka/CooworkaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CooworkaApp.swift
+//  Cooworka
+//
+//  Created by Natasha Hartanti Winata on 13/06/24.
+//
+
+import SwiftUI
+
+@main
+struct CooworkaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
