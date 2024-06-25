0a7d7cf142fd90c65087a1e87ef6a4e2cdfa1007
Cindaa10 <cindaarta16@gmail.com>
2024-06-25 14:42:06 +0700

    Integrate Profile

--- Cooworka/Cooworka/Views/LevelProfile/LevelProfileModel.swift
+++ Cooworka/Cooworka/Views/LevelProfile/LevelProfileModel.swift
@@ -10,4 +10,18 @@ import Foundation
 struct LevelProfileModel {
     var imageName: String
     var pointsToUnlock: Int
+    var noLevel: Int
 }
+
+var Levels: [LevelProfileModel] = [
+    .init(imageName: "BudakJompo", pointsToUnlock: 1000, noLevel: 1),
+    .init(imageName: "Unknown", pointsToUnlock: 2000, noLevel: 2),
+    .init(imageName: "Unknown", pointsToUnlock: 4000, noLevel: 3),
+    .init(imageName: "Unknown", pointsToUnlock: 8000, noLevel: 4),
+    .init(imageName: "Unknown", pointsToUnlock: 16000, noLevel: 5),
+    .init(imageName: "Unknown", pointsToUnlock: 32000, noLevel: 6),
+    .init(imageName: "Unknown", pointsToUnlock: 64000, noLevel: 7),
+    .init(imageName: "Unknown", pointsToUnlock: 128000, noLevel: 8),
+    .init(imageName: "Unknown", pointsToUnlock: 256000, noLevel: 9)
+         
+]
