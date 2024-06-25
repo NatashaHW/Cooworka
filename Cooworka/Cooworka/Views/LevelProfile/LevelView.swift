aec19f9547d8d3a00f5f255189ccc64b1aafa33e
Cindaa10 <cindaarta16@gmail.com>
2024-06-25 14:42:06 +0700

    Integrate Profile

--- Cooworka/Cooworka/Views/LevelProfile/LevelView.swift
+++ Cooworka/Cooworka/Views/LevelProfile/LevelView.swift
@@ -11,6 +11,10 @@ struct LevelView: View {
     @State var descPangkat = "Pangkat kamu saat ini adalah..."
     @State var label = "Budak Jompo"
     
+    @State var currentLevel = 1
+    
+    let levels: [LevelProfileModel]
+    
     var body: some View {
         VStack{
             ZStack{
@@ -50,13 +54,104 @@ struct LevelView: View {
                     }
                     .padding(.bottom, 24)
                     
-//                    Image("BudakJompo")
-//                        .resizable()
-//                        .frame(width: 265, height: 265)
-//                        .aspectRatio(contentMode: .fit)
-                    
-                    Circle()
-                        .frame(width: .infinity, height: 330)
+                    ZStack{
+                        HStack(spacing: 150){
+                            Button(action: {
+                                if currentLevel != 1 {
+                                    currentLevel -= 1
+                                }
+                            }, label: {
+                                Image(systemName: "chevron.left")
+                                    .resizable()
+                                    .frame(width: 8, height: 13)
+                                    .foregroundColor(.black)
+                            })
+                            
+                            Button(action: {
+                                if currentLevel != 9 {
+                                    currentLevel += 1
+                                }
+                            }, label: {
+                                Image(systemName: "chevron.right")
+                                    .resizable()
+                                    .frame(width: 8, height: 13)
+                                    .foregroundColor(.black)
+                            })
+                            
+                            
+                        }
+                        .zIndex(1)
+                        .offset(y: 100)
+                        
+                        ScrollViewReader { proxy in
+                            ScrollView(.horizontal){
+                                VStack (alignment: .leading){
+                                    HStack(spacing: 75) {
+                                        ForEach(levels, id: \.noLevel) { level in
+                                            Image(level.imageName)
+                                                .resizable()
+                                                .aspectRatio(contentMode: .fit)
+                                                .frame(width: 175)
+                                                .scaleEffect(currentLevel == level.noLevel ? 1.1 : 0.9)
+                                            
+                                        }
+                                    }
+                                    .padding(.leading, 110)
+                                    .padding(.bottom, 20)
+                                    .padding(.top, 20)
+                                    
+                                    
+                                    
+                                    
+                                    ZStack (alignment: .leading){
+                                        RoundedRectangle(cornerRadius: 25)
+                                            .frame(width: 2070/170, height: 10) //bagi point xp
+                                            .padding()
+                                            .zIndex(0.5)
+                                            .overlay(
+                                                LinearGradient(gradient: Gradient(colors: [.red, .blue]), startPoint: .leading, endPoint: .trailing))
+                                            .mask(RoundedRectangle(cornerRadius: 25)
+                                                .frame(width: 2070/170, height: 10))
+                                        
+                                        
+                                        
+                                        RoundedRectangle(cornerRadius: 25)
+                                            .frame(width: 2070, height: 14)
+                                            .padding(.horizontal, 15)
+                                            .foregroundColor(.white)
+                                        
+                                        
+                                        HStack(spacing: 205){
+                                            ForEach(levels, id: \.noLevel){ level in
+                                                ZStack {
+                                                    Image("PointBorder")
+                                                        .resizable()
+                                                        .frame(width: 46, height: 46)
+                                                    
+                                                    
+                                                    Text("\(level.pointsToUnlock) XP")
+                                                        .multilineTextAlignment(.center)
+                                                        .frame(width: 40)
+                                                        .font(.system(size: 7, weight: .regular))
+                                                        .offset(y: 2)
+                                                }
+                                                
+                                            }
+                                            
+                                        }
+                                        .padding(.leading, 178)
+                                        .zIndex(1)
+                                    }
+                                }
+                            }
+                            .disabled(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                            .onChange(of: currentLevel) { newValue in
+                                withAnimation {
+                                    proxy.scrollTo(newValue, anchor: .center)
+                                }
+                            }
+                        }
+                    }
                     
                     Text("Kumpulkan 110 XP lagi untuk naik pangkat!")
                         .font(.subheadline)
@@ -102,5 +197,10 @@ struct LevelView: View {
 }
 
 #Preview {
-    LevelView()
+    LevelView(levels: [LevelProfileModel(imageName: "BudakJompo", pointsToUnlock: 256000, noLevel: 1),
+                       LevelProfileModel(imageName: "Unknown", pointsToUnlock: 512000, noLevel: 2),
+                       LevelProfileModel(imageName: "Unknown", pointsToUnlock: 512000, noLevel: 3)
+                       
+                       
+                      ])
 }
